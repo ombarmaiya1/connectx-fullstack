97eef481aa4339da909cbbb0b69f697eac9ff407
Om <ombarmaiyacontact@gmail.com>
2026-01-20 18:34:15 +0530

    Remove explicit password validator from serializer

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -6,7 +6,7 @@ User = get_user_model()
 
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password = serializers.CharField(write_only=True, required=True)
     password2 = serializers.CharField(write_only=True, required=True)
     name = serializers.CharField(write_only=True, required=False)
 
